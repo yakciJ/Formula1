df7a6b65f8dd33548a0c20106a93b037ba965569
Huang <154526787+GnaoHz@users.noreply.github.com>
2024-09-13 15:46:45 +0700

    add image driver

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 3000;
-
+const port = 3001;
 // Serve static files from the 'FrontEnd' directory
 app.use(express.static(path.join(__dirname, 'FrontEnd')));
-
+app.use('/Image', express.static(path.join(__dirname, 'Image'))); // Serve images from 'Image' directory
 // Serve the main HTML file
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'FrontEnd', 'home.html'));
