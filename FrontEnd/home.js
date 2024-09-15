7c64f4de1bb42cc1af6c31f9a169da58e4d78951
Huang <154526787+GnaoHz@users.noreply.github.com>
2024-09-15 22:39:41 +0700

    add img team + click thongtin

--- FrontEnd/home.js
+++ FrontEnd/home.js
@@ -1,4 +1,18 @@
 document.getElementById('tay-dua').addEventListener('click', function(event) {
   event.preventDefault();
+  document.querySelectorAll('body > main > section').forEach(function(section) {
+    section.style.display = 'none';
+  });
   document.getElementById('sec-f1-driver').style.display = 'block';
 });
+
+document.querySelector('body>header>section #doi-dua').addEventListener('click', function(event) {
+  event.preventDefault();
+  document.querySelectorAll('body > main > section').forEach(function(section) { 
+    section.style.display = 'none';
+  });
+  document.querySelector('body > main #sec-f1-team').style.display = 'block'; 
+});
+
+
+
