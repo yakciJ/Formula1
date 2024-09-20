65738c0fc410220a552498dc6c6d10ddd8a11940
Jickay <sieunhan482@gmail.com>
2024-09-20 14:11:14 +0700

    Split backend
    add cluster to prevent server crashing and split backend to tracks.js

---
+++ BackEnd/tracks.js
@@ -0,0 +1,42 @@
+const exp = require('constants');
+const express = require('express');
+const path = require('path');
+const app = express();
+
+
+var database = require('mysql');
+var FrontEnd = __dirname+'/../FrontEnd';
+
+//Connect to database. Need: npm install mysql
+var con = database.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    port: 3306
+});
+
+module.exports = function (app) {
+    app.get('/test', function(req, res) {
+        res.json(200, {'test': 'it works!'})
+    });
+
+    // To the tracks page
+    app.get('/duong-dua', (req, res) => {
+        res.sendFile(path.join(FrontEnd, 'Tracks', 'tracks.html'));
+    });
+
+    // Connect to db
+    app.get('/connect', (req, res) => {
+        con.connect(function(err) {
+            if (err) throw err;
+            console.log("Connected!");
+        });
+        
+    });
+}
+
+
+
+
+
+
--- server.js
+++ server.js
@@ -1,39 +1,51 @@
-const exp = require('constants');
-const express = require('express');
-const path = require('path');
-const app = express();
-const port = 6969;
+var cluster = require('cluster');
+// Make server reset every time it crash.
+if(cluster.isMaster){
 
-var database = require('mysql');
-var FrontEnd = __dirname+'/FrontEnd';
+    // Count the machine's CPUs
+    var cpuCount = require('os').cpus().length;
 
-// Serve static files from the directories
-app.use('/FrontEnd', express.static(path.join(__dirname, 'FrontEnd')));
-app.use('/Image', express.static(path.join(__dirname, 'Image')));
-app.use('/BackEnd', express.static(path.join(__dirname, 'BackEnd')))
-// Serve the main HTML file
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'FrontEnd', 'Home', 'home.html'));
-});
-// To the tracks page
-app.get('/duong-dua', (req, res) => {
-    res.sendFile(path.join(FrontEnd, 'Tracks', 'tracks.html'));
-});
+    // Create a worker for each CPU
+    for (var i = 0; i < cpuCount; i += 1) {
+        cluster.fork();
+    }
 
-// Start the server
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-});
-//Connect to database. Need: npm install mysql
-var con = database.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    port: 3306
-});
-app.get('/connect', (req, res) => {
-    con.connect(function(err) {
-        if (err) throw err;
-        console.log("Connected!");
+    cluster.on('exit', function (worker) {
+
+        // Replace the dead worker,
+        // we're not sentimental
+        console.log('Worker %d died :(', worker.id)+'. Reset server!';
+        cluster.fork();
+    
     });
-});
+} 
+else
+{
+    const exp = require('constants');
+    const express = require('express');
+    const path = require('path');
+    const app = express();
+    const port = 6969;
+    
+    var database = require('mysql');
+    var FrontEnd = __dirname+'/FrontEnd';
+    
+    // Serve static files from the directories
+    app.use('/FrontEnd', express.static(path.join(__dirname, 'FrontEnd')));
+    app.use('/Image', express.static(path.join(__dirname, 'Image')));
+    app.use('/BackEnd', express.static(path.join(__dirname, 'BackEnd')))
+    // Serve the main HTML file
+    app.get('/', (req, res) => {
+        res.sendFile(path.join(__dirname, 'FrontEnd', 'Home', 'home.html'));
+    });
+    
+    
+    // Start the server
+    app.listen(port, () => {
+        console.log(`Server is running on http://localhost:${port}`);
+    });
+    
+    // Connect to backend tracks.js
+    require(path.join(__dirname, '/BackEnd/tracks.js'))(app);
+}
+
