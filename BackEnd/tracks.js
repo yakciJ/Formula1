9db807150b95b875c85efc691c8168b11edcdaff
Jickay <sieunhan482@gmail.com>
2024-09-28 23:57:36 +0700

    add back to tracks button

--- BackEnd/tracks.js
+++ BackEnd/tracks.js
@@ -6,14 +6,8 @@ var FrontEnd = __dirname+'/../FrontEnd';
 app.set("view engine", "ejs");
 
 module.exports = function (app) {
-
-    // test html
-    app.get('/testhtml', (req, res) => {
-        res.sendfile(path.join(FrontEnd, 'Tracks', 'tracks.html'));
-    });
-
     // render tracks.ejs
-    app.get('/duong-dua', async (req,res) => {
+    app.get('/duong-dua', (req,res) => {
         const con = require('./dbcon');
         con.query('Select ID, Name, BasicImage from tracks', function(err, result, fields){
             if(err){
@@ -23,7 +17,7 @@ module.exports = function (app) {
             }
         });
     })
-
+    // render track.ejs
     app.get('/duong-dua/:id', (req, res) => {
         console.log(req.params.id);
         const con = require('./dbcon');
